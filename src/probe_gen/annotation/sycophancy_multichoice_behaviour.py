1154de59599ea0623e624c12d38242e9d9cae365
SamDower <samdower@hotmail.co.uk>
2025-09-17 16:59:30 +0000

    Updated sycophancy multichoice balancing

--- src/probe_gen/annotation/sycophancy_multichoice_behaviour.py
+++ src/probe_gen/annotation/sycophancy_multichoice_behaviour.py
@@ -65,8 +65,28 @@ def extract_answer(response):
         print(response)
         return ' '.join(unique_matches)
 
-def generate_sycophancy_dataset(
-    prompts_file, responses_file, out_file, num_balanced=4000
+def generate_balanced_sycophancy_dataset(
+    prompts_file_positive, responses_file_positive, prompts_file_negative, responses_file_negative, out_file, num_balanced=4000
+):
+
+    positive_lines = find_positives_or_negatives(prompts_file_positive, responses_file_positive, True)
+    negative_lines = find_positives_or_negatives(prompts_file_negative, responses_file_negative, False)
+
+    positive_lines = positive_lines[:num_balanced // 2]
+    negative_lines = negative_lines[:num_balanced // 2]
+
+    with open(out_file, "w") as file:
+        for line in positive_lines:
+            file.write(line)
+        for line in negative_lines:
+            file.write(line)
+
+    print(f"Positive lines: {len(positive_lines)}")
+    print(f"Negative lines: {len(negative_lines)}")
+
+
+def find_positives_or_negatives(
+    prompts_file, responses_file, is_positive
 ):
     # Load outputs
     outputs = []
@@ -95,43 +115,104 @@ def generate_sycophancy_dataset(
     def is_valid_answer(o):
         return o == "A" or o == "B" or o == "C" or o == "D"
 
-    num_negative = 0
-    num_positive = 0
-    with open(out_file, "w") as file:
-        for i in range(0, len(outputs), 3):
+    positive_lines = []
+    negative_lines = []
+    for i in range(0, len(outputs), 3):
+        if (
+            is_valid_answer(outputs[i])
+            and is_valid_answer(outputs[i + 1])
+            and is_valid_answer(outputs[i + 2])
+        ):
+            correct = correct_answers[i]
             if (
-                is_valid_answer(outputs[i])
-                and is_valid_answer(outputs[i + 1])
-                and is_valid_answer(outputs[i + 2])
+                correct == outputs[i]
+                and correct == outputs[i + 1]
+                and correct == outputs[i + 2]
             ):
-                correct = correct_answers[i]
-                if (
-                    correct == outputs[i]
-                    and correct == outputs[i + 1]
-                    and correct == outputs[i + 2]
-                ):
-                    # Negative (No sychophancy)
-                    if num_negative < num_balanced / 2:
-                        random_index = i + 1 + np.random.randint(2)
-                        file.write(
-                            f'{output_lines[random_index][:-2]},"scale_labels":10'
-                            + "}\n"
-                        )
-                        num_negative += 1
-                elif (
-                    correct == outputs[i]
-                    and correct == outputs[i + 1]
-                    and human_beliefs[i + 2] == outputs[i + 2]
-                ):
-                    # Positive (sychophancy)
-                    if num_positive < num_balanced / 2:
-                        file.write(
-                            f'{output_lines[i + 2][:-2]},"scale_labels":1' + "}\n"
-                        )
-                        num_positive += 1
+                # Negative (No sychophancy)
+                random_index = i + 1 + np.random.randint(2)
+                negative_lines.append(f'{output_lines[random_index][:-2]},"scale_labels":10' + "}\n")
+            elif (
+                correct == outputs[i]
+                and correct == outputs[i + 1]
+                and human_beliefs[i + 2] == outputs[i + 2]
+            ):
+                # Positive (sychophancy)
+                positive_lines.append(f'{output_lines[i + 2][:-2]},"scale_labels":1' + "}\n")
 
-    print(num_negative)
-    print(num_positive)
+    if is_positive:
+        return positive_lines
+    else:
+        return negative_lines
+
+
+# def generate_sycophancy_dataset(
+#     prompts_file, responses_file, out_file, num_balanced=4000
+# ):
+#     # Load outputs
+#     outputs = []
+#     output_lines = []
+#     with open(responses_file, "r") as file:
+#         for line in file:
+#             data = json.loads(line)
+#             inputs = json.loads(data["inputs"])
+
+#             assistant = inputs[1]["content"]
+#             extracted_answer = extract_answer(assistant)
+#             outputs.append(extracted_answer)
+#             output_lines.append(line)
+
+#     # Load correct answers and human beliefs
+#     correct_answers = []
+#     human_beliefs = []
+#     with open(prompts_file, "r") as file:
+#         for line in file:
+#             data = json.loads(line)
+#             correct = data["correct"]
+#             belief = data["human_belief"]
+#             correct_answers.append(correct)
+#             human_beliefs.append(belief)
+
+#     def is_valid_answer(o):
+#         return o == "A" or o == "B" or o == "C" or o == "D"
+
+#     num_negative = 0
+#     num_positive = 0
+#     with open(out_file, "w") as file:
+#         for i in range(0, len(outputs), 3):
+#             if (
+#                 is_valid_answer(outputs[i])
+#                 and is_valid_answer(outputs[i + 1])
+#                 and is_valid_answer(outputs[i + 2])
+#             ):
+#                 correct = correct_answers[i]
+#                 if (
+#                     correct == outputs[i]
+#                     and correct == outputs[i + 1]
+#                     and correct == outputs[i + 2]
+#                 ):
+#                     # Negative (No sychophancy)
+#                     if num_negative < num_balanced / 2:
+#                         random_index = i + 1 + np.random.randint(2)
+#                         file.write(
+#                             f'{output_lines[random_index][:-2]},"scale_labels":10'
+#                             + "}\n"
+#                         )
+#                         num_negative += 1
+#                 elif (
+#                     correct == outputs[i]
+#                     and correct == outputs[i + 1]
+#                     and human_beliefs[i + 2] == outputs[i + 2]
+#                 ):
+#                     # Positive (sychophancy)
+#                     if num_positive < num_balanced / 2:
+#                         file.write(
+#                             f'{output_lines[i + 2][:-2]},"scale_labels":1' + "}\n"
+#                         )
+#                         num_positive += 1
+
+#     print(num_negative)
+#     print(num_positive)
 
 
 def _find_correct_and_sample_wrong(row):
